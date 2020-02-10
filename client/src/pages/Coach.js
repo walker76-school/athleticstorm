1c420837aaaca502aae66f841836dbea0dc26a7d
John Eyre <John_Eyre@baylor.edu>
2020-02-10 14:52:58 -0600

    Minimal information showing up on the page

--- client/src/pages/Coach.js
+++ client/src/pages/Coach.js
@@ -7,15 +7,31 @@ import '../common/AppHeader.css';
 class Coach extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            coach_first_name: [],
+            coach_last_name: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://api.collegefootballdata.com/coaches?minYear=2000')
+            .then(res => res.json())
+            .then(result => this.setState({ coach_first_name: result[0].first_name, coach_last_name: result[0].last_name }))
+        console.log("state", this.state.coach_first_name);
     }
 
     render() {
         return (
             <div>
-                <h1>Coach</h1>
-                <p>This page gives info about a coach in specific.</p>
+                <center><h1>Coaches</h1></center>
+                    <div className="card">
+                        <div className="card-body">
+                        <h2>{this.state.coach_first_name} {this.state.coach_last_name}</h2>
+                        <h5></h5>
+                        </div>
+                    </div>
             </div>
-        );
+        )
     }
 }
 
