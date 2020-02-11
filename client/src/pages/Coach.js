89061e4af204fb86326fd683b3f2311e82415bda
John Eyre <John_Eyre@baylor.edu>
2020-02-10 19:15:07 -0600

    Coach url works; displays all head coach seasons with record

--- client/src/pages/Coach.js
+++ client/src/pages/Coach.js
@@ -1,35 +1,61 @@
-import React, { Component } from 'react';
-import {
-    withRouter
-} from 'react-router-dom';
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
 import '../common/AppHeader.css';
+import axios from 'axios';
 
 class Coach extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            coach_first_name: [],
-            coach_last_name: []
-        }
+    }
+
+    state = {
+        first_name: "",
+        last_name: "",
+        seasonList: [{
+            school: "",
+            year: "",
+            wins: 0,
+            losses: 0,
+            preseason_rank: null,
+            postseason_rank: null
+        }]
+};
+
+
+    coachUrlExtractor() {
+        let name = window.location.pathname.substr(7);
+        return name.split('-');
     }
 
     componentDidMount() {
-        fetch('https://api.collegefootballdata.com/coaches?minYear=2000')
-            .then(res => res.json())
-            .then(result => this.setState({ coach_first_name: result[0].first_name, coach_last_name: result[0].last_name }))
-        console.log("state", this.state.coach_first_name);
+        // Get coach name from url
+        let names = this.coachUrlExtractor();
+        axios.get('https://api.collegefootballdata.com/coaches?firstName=' +
+            names[0] + '&lastName=' + names[1] + '&minYear=2000')
+            .then(res => {
+                const coaches = res.data;
+                console.log(coaches[0]);
+                this.setState({
+                    first_name: coaches[0].first_name,
+                    last_name: coaches[0].last_name,
+                    seasonList: coaches[0].seasons
+                });
+            })
     }
 
     render() {
         return (
             <div>
-                <center><h1>Coaches</h1></center>
-                    <div className="card">
-                        <div className="card-body">
-                        <h2>{this.state.coach_first_name} {this.state.coach_last_name}</h2>
-                        <h5></h5>
-                        </div>
-                    </div>
+                <div className="Coach_Info">
+                    <h1>{this.state.first_name} {this.state.last_name}</h1>
+                </div>
+                <div className="Seasons">
+                    <h3>
+                    {this.state.seasonList.map((season) => (
+                        <p>{season.school} ({season.year}): {season.wins}-{season.losses}</p>
+                    ))}
+                    </h3>
+                </div>
             </div>
         )
     }
