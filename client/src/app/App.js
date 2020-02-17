0ca37b4d35b71283a4561a27cbe52a17a580f9c9
Joshua Pane <joshua_pane@baylor.edu>
2020-02-17 11:29:08 -0600

    Added reference to setSchool Function

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -108,7 +108,7 @@ class App extends Component {
           <Content className="app-content">
             <div className="container">
               <Switch>      
-                <Route exact path="/" render={(props) => <Home isAuthenticated={this.state.isAuthenticated} currentUser={this.state.currentUser} handleLogout={this.handleLogout} {...props} />}/>
+                <Route exact path="/" render={(props) => <Home changeSchool={this.setSchool} isAuthenticated={this.state.isAuthenticated} currentUser={this.state.currentUser} handleLogout={this.handleLogout} {...props} />}/>
                 <Route path="/school/:schoolName" render={(props) => <School school={this.state.clickedSchool} isAuthenticated={this.state.isAuthenticated}  currentUser={this.state.currentUser} handleLogout={this.handleLogout} {...props} />}/>
                 <Route path="/login" render={(props) => <Login onLogin={this.handleLogin} {...props} />}/>
                 <Route path="/signup" component={Signup} />
