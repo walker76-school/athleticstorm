b9e324ea2df8b1808dc78ee2cee860ebf6b66be9
John Eyre <jmeyre1998@gmail.com>
2020-02-16 21:23:04 -0600

    hardening for coach name url

--- client/src/pages/Coach.js
+++ client/src/pages/Coach.js
@@ -19,12 +19,28 @@ class Coach extends Component {
             preseason_rank: null,
             postseason_rank: null
         }]
-};
+    };
 
 
     coachUrlExtractor() {
         let name = window.location.pathname.substr(7);
         return name.split('-');
+    }
+
+    allTimeWins() {
+        let wins = 0;
+        for (let i = 0; i < this.state.seasonList.length; i++) {
+            wins += this.state.seasonList[i].wins;
+        }
+        return wins;
+    }
+
+    allTimeLosses() {
+        let losses = 0;
+        for (let i = 0; i < this.state.seasonList.length; i++) {
+            losses += this.state.seasonList[i].losses;
+        }
+        return losses;
     }
 
     componentDidMount() {
@@ -35,29 +51,46 @@ class Coach extends Component {
             .then(res => {
                 const coaches = res.data;
                 console.log(coaches[0]);
-                this.setState({
-                    first_name: coaches[0].first_name,
-                    last_name: coaches[0].last_name,
-                    seasonList: coaches[0].seasons
-                });
+                if (coaches.length !== 0) {
+                    this.setState({
+                        first_name: coaches[0].first_name,
+                        last_name: coaches[0].last_name,
+                        seasonList: coaches[0].seasons
+                    });
+                }
             })
     }
 
     render() {
-        return (
-            <div>
-                <div className="Coach_Info">
-                    <h1>{this.state.first_name} {this.state.last_name}</h1>
+        if (this.state.first_name.length !== 0) {
+
+            // Name successfully found
+            return (
+                <div>
+                    <div className="Coach_Info">
+                        <h1>{this.state.first_name} {this.state.last_name}</h1>
+                        <h2>All time record: {this.allTimeWins()}-{this.allTimeLosses()}</h2>
+                    </div>
+                    <div className="Seasons">
+                        <h3>
+                            {this.state.seasonList.map((season) => (
+                                <p>{season.school} ({season.year}): {season.wins}-{season.losses}</p>
+                            ))}
+                        </h3>
+                    </div>
                 </div>
-                <div className="Seasons">
-                    <h3>
-                    {this.state.seasonList.map((season) => (
-                        <p>{season.school} ({season.year}): {season.wins}-{season.losses}</p>
-                    ))}
-                    </h3>
+            )
+        } else {
+
+            // Error: name not found
+            return (
+                <div>
+                    <div className="Error_Header">
+                        <h1>Coach Not Found</h1>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }
     }
 }
 
