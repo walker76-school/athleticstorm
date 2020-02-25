729832f4c6121ef702481a8535ef80a210d885ee
EBAKE <Evan_Baker@baylor.edu>
2020-02-24 20:00:38 -0600

    First addition of infrastructure to add roles

--- server/src/main/java/edu/baylor/ecs/athleticstorm/payload/UserSummary.java
+++ server/src/main/java/edu/baylor/ecs/athleticstorm/payload/UserSummary.java
@@ -2,10 +2,12 @@ package edu.baylor.ecs.athleticstorm.payload;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import ../util/AppConstants.java;
 
 @Data
 @AllArgsConstructor
 public class UserSummary {
     private Long id;
     private String username;
+    private Role role;
 }
--- server/src/main/java/edu/baylor/ecs/athleticstorm/util/AppConstants.java
+++ server/src/main/java/edu/baylor/ecs/athleticstorm/util/AppConstants.java
@@ -5,4 +5,9 @@ public interface AppConstants {
     String DEFAULT_PAGE_SIZE = "30";
 
     int MAX_PAGE_SIZE = 50;
+
+    enum Role {
+        REDSHIRT, STARTER, MVP;
+    }
+
 }
