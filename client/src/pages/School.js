142fdfecda0caa532de3b02d5f80f1d657b9fadb
Joshua Pane <joshua_pane@baylor.edu>
2020-02-25 09:47:55 -0600

    fixed going straight to endpoint works now, you dont have to go through home page to get to a team

--- client/src/pages/School.js
+++ client/src/pages/School.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-    withRouter, Redirect
+    withRouter
 } from 'react-router-dom';
 import '../common/AppHeader.css';
 import logo from './football.jpeg'
@@ -18,50 +18,50 @@ class School extends Component {
         year: '2019'
     }
 
-    descSort(){
+    descSort() {
         const sortedCoaches = [].concat(this.state.coaches).sort((a, b) => a.last_name > b.last_name ? 1 : -1);
         this.setState({ coaches: sortedCoaches });
     }
 
-    ascSort(){
-        const sortedCoaches = [].concat(this.state.coaches).sort((a, b) => a.last_name  < b.last_name ? 1 : -1);
+    ascSort() {
+        const sortedCoaches = [].concat(this.state.coaches).sort((a, b) => a.last_name < b.last_name ? 1 : -1);
         this.setState({ coaches: sortedCoaches });
     }
 
-    mostRecentSort(){
-        const sortedCoaches = [].concat(this.state.coaches).sort((a, b) => parseInt(a.seasons[0].year, 10)  < parseInt(b.seasons[0].year, 10) ? 1 : -1);
+    mostRecentSort() {
+        const sortedCoaches = [].concat(this.state.coaches).sort((a, b) => parseInt(a.seasons[0].year, 10) < parseInt(b.seasons[0].year, 10) ? 1 : -1);
         this.setState({ coaches: sortedCoaches });
     }
 
-    oldestSort(){
+    oldestSort() {
         const sortedCoaches = [].concat(this.state.coaches).sort((a, b) => parseInt(a.seasons[0].year, 10) > parseInt(b.seasons[0].year, 10) ? 1 : -1);
         this.setState({ coaches: sortedCoaches });
     }
 
-    bestScoreSort(){
+    bestScoreSort() {
 
     }
 
-    worstScoreSort(){
+    worstScoreSort() {
 
     }
 
-    coachSort = (event) =>{
+    coachSort = (event) => {
         let sortBy = event.target.value;
         console.log(event.target.value);
-        if("Descending" === sortBy){
+        if ("Descending" === sortBy) {
             this.descSort();
-        }else if("Ascending" === sortBy){
+        } else if ("Ascending" === sortBy) {
             this.ascSort();
-        }else if("Most Recent" === sortBy){
+        } else if ("Most Recent" === sortBy) {
             this.mostRecentSort();
-        }else if("Oldest" === sortBy){
+        } else if ("Oldest" === sortBy) {
             this.oldestSort();
-        }else if("Best Score" === sortBy){
+        } else if ("Best Score" === sortBy) {
             this.bestScoreSort();
-        }else if("Worst Score" === sortBy){
+        } else if ("Worst Score" === sortBy) {
             this.worstScoreSort();
-        }else{
+        } else {
             console.log("Invalid Option " + sortBy);
         }
     }
@@ -73,33 +73,27 @@ class School extends Component {
         // Get Team Color Scheme And Name From API
         if (this.props.school) {
             this.setState({ logo: this.props.school.logos[0], primaryColor: this.props.school.color, secondaryColor: this.props.school.alt_color })
-        }else{
-            return;
+        } else {
+                 // Get List Of Coaches From API
+            axios.get('https://api.collegefootballdata.com/teams/fbs')
+            .then(res => {
+                const teamList = res.data;
+                for(var x = 0; x < teamList.length; x++){
+                    const teamName = window.location.pathname.replace('%20', ' ').substr(8).toUpperCase();
+                    if(teamName === teamList[x].school.toUpperCase()){
+                        this.setState({ logo: teamList[x].logos[0], primaryColor: teamList[x].color, secondaryColor: teamList[x].alt_color })
+                    }
+                }
+            });
         }
         // Get List Of Coaches From API
-        axios.get('https://api.collegefootballdata.com/coaches?team=' + window.location.pathname.substr(8) + '&minYear=2000')
+        axios.get('https://api.collegefootballdata.com/coaches?team=' + window.location.pathname.substr(8) + '&minYear=2017')
             .then(res => {
                 const coachlist = res.data;
                 this.setState({ coaches: coachlist });
             });
-        // Get List Of Players From API
-        axios.get('https://api.collegefootballdata.com/roster?team=' + window.location.pathname.substr(8) + '&year=' + this.state.year)
-            .then(res => {
-                const allplayerlist = res.data;
-                var playerlist = [];
-                if (allplayerlist.length <= 9) {
-                    this.setState({ players: res.data });
-                } else {
-                    console.log(allplayerlist[0]);
-                    for (var x = 0; x < 7; x++) {
-                        if (allplayerlist[x].first_name !== null) {
-                            playerlist[x] = allplayerlist[x];
-                        }
-                    }
-                    this.setState({ players: playerlist });
-                }
-            })
         
+        this.getPlayers(2019);
     }
 
 
@@ -125,29 +119,33 @@ class School extends Component {
     }
 
     getPlayers(playerYear) {
+        this.setState({ year: playerYear });
         // Get List Of Players From API
-        axios.get('https://api.collegefootballdata.com/roster?team=' + window.location.pathname.substr(8) + '&year=' + playerYear)
+        axios.get('https://api.collegefootballdata.com/player/usage?year=' + this.state.year + '&team=' + window.location.pathname.substr(8) + '&excludeGarbageTime=true')
             .then(res => {
                 const allplayerlist = res.data;
                 var playerlist = [];
                 if (allplayerlist.length <= 9) {
                     this.setState({ players: res.data });
                 } else {
-                    for (var x = 0; x < 7; x++) {
-                        if (allplayerlist[x].first_name !== null) {
-                            playerlist[x] = allplayerlist[x];
+                    for (var y = 0; y < allplayerlist.length/7; y++) {
+                        var tempList = [];
+                        for (var x = 0; x < 7 && y*7+x < allplayerlist.length; x++) {
+                            if (allplayerlist[y*7+x].first_name !== null) {
+                                tempList[x] = allplayerlist[y*7+x];
+                            }
                         }
+                        playerlist[y] = tempList;
                     }
                     this.setState({ players: playerlist });
-                    this.setState({ year: playerYear });
                 }
             })
     }
 
     render() {
-        if (!this.props.school || window.location.pathname.replace('%20', ' ').substr(8) !== this.props.school.school) {
-            return (<Redirect to='/' />)
-        }
+        // if (!this.props.school && this.state.logo === '') {
+        //     return (<Redirect to='/' />)
+        // }
         return (
             <div>
                 <div className="School_Info">
@@ -190,37 +188,38 @@ class School extends Component {
                             <option value="2019">2019</option>
                             <option value="2018">2018</option>
                             <option value="2017">2017</option>
-                            <option value="2016">2016</option>
                         </select>
                     </h1>
                     <table className="table">
-                        <thead>
-                            <tr>
-                                {/* Get Coaches Images */}
-                                {this.state.players.map((player, i) => (
-                                    <th width="150" key={i}>
-                                        <Link to={`/player/${player.first_name}_${player.last_name}`} style={{ color: this.state.primaryColor }} >
-                                            <center><img src={logo} width="100" height="50" alt={`${player.first_name} ${player.last_name}`} /></center>
-                                        </Link>
-                                    </th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                {/* Get Coach Name & Year */}
-                                {this.state.players.map((player, i) => (
-                                    <td width="150" key={i}>
-                                        <Link to={`/player/${player.first_name}_${player.last_name}`} style={{ color: this.state.primaryColor }} >
-                                            <center >{player.first_name + " " + player.last_name}
-                                                <br></br>
-                                                {player.position ? player.position + " " + this.state.year : this.state.year}
-                                            </center>
-                                        </Link>
-                                    </td>
-                                ))}
-                            </tr>
-                        </tbody>
+
+
+                        {/* Get Player Images */}
+                        {this.state.players.map((playery, x) => (
+                            <tbody key={x}>
+                                <tr>
+                                    {playery.map((player, i) => (
+                                        <td width="150" key={i}>
+                                            <Link to={`/player/${player.first_name}_${player.last_name}`} style={{ color: this.state.primaryColor }} >
+                                                <center><img src={logo} width="100" height="50" alt={`${player.first_name} ${player.last_name}`} /></center>
+                                            </Link>
+                                        </td>
+                                    ))}
+                                </tr>
+                                <tr>
+                                    {playery.map((player, i) => (
+                                        <td width="150" key={i}>
+                                            <Link to={`/player/${player.name.substr(0, player.name.indexOf(' '))}_${player.name.substr(player.name.indexOf(' ') + 1)}`} style={{ color: this.state.primaryColor }} >
+                                                <center >{player.name}
+                                                    <br></br>
+                                                    {player.position ? player.position + " " + this.state.year : this.state.year}
+                                                </center>
+                                            </Link>
+                                        </td>
+                                    ))}
+                                </tr>
+
+                            </tbody>
+                        ))}
                     </table>
                 </div>
             </div>
