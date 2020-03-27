e2f7c782c0b01cf9b9236e9c8bf38448edfc7ccd
EBAKE <Evan_Baker@baylor.edu>
2020-03-27 15:19:14 -0500

    Still trying to fix issue of getCurrentUser(), changed User.java to only using subRole to store subscription role rather than 'roles'

--- server/src/main/java/edu/baylor/ecs/athleticstorm/security/UserPrincipal.java
+++ server/src/main/java/edu/baylor/ecs/athleticstorm/security/UserPrincipal.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -29,9 +30,7 @@ public class UserPrincipal implements UserDetails {
     }
 
     public static UserPrincipal create(User user) {
-        List<GrantedAuthority> authorities = user.getRoles().stream().map(role ->
-                new SimpleGrantedAuthority(role.getName().name())
-        ).collect(Collectors.toList());
+        List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(user.getSubRole()));
 
         return new UserPrincipal(
                 user.getId(),
