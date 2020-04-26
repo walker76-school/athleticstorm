77137272d1af42833881c986327d63e099949e29
John Eyre <jmeyre1998@gmail.com>
2020-04-26 17:49:58 -0500

    Visual front page changes

--- client/src/common/AppHeader.js
+++ client/src/common/AppHeader.js
@@ -90,10 +90,10 @@ class AppHeader extends Component {
             }
           menuItems = [
             <Menu.Item key="/login">
-              <Link to="/login">Login</Link>
+              <Link to="/login">Log In</Link>
             </Menu.Item>,
             <Menu.Item key="/signup">
-              <Link to="/signup">Signup</Link>
+              <Link to="/signup">Sign Up</Link>
             </Menu.Item>
         ];
         }
